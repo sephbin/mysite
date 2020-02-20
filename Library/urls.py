dc9edf03a0bb07a86a78cc109047e6f63b82d99c
Andrew Butler <s-abutler@cox.com.au>
2020-02-20 15:53:16 +1100

    added views

--- Library/urls.py
+++ Library/urls.py
@@ -6,6 +6,8 @@ router = routers.DefaultRouter()
 router.register(r'things',		views.ThingViewSet)
 
 urlpatterns = [
+	path(r'choosething/<str:thingtype>/<str:srating>/<str:gcolour>/<str:choice>/', views.getOneThing),
+
 	url(r'^', include(router.urls)),
 	url(r'^api-auth/', include('rest_framework.urls')),
 ]
--- Library/views.py
+++ Library/views.py
@@ -4,6 +4,8 @@ from .serializers import *
 from rest_framework import viewsets
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import get_object_or_404, render, redirect
 # Create your views here.
 class ThingViewSet(viewsets.ModelViewSet):
 	queryset = thing.objects.all()
@@ -11,3 +13,11 @@ class ThingViewSet(viewsets.ModelViewSet):
 	filter_backends = [filters.SearchFilter , DjangoFilterBackend]
 	filterset_fields = ["generalColour__name",]
 	search_fields = ("name",)
+
+
+def getOneThing(reguest, thingtype, srating, gcolour, choice)
+
+	thingtype = get_object_or_404(thingSubType, name=thingtype)
+	gcolour = get_object_or_404(generalColour, name=gcolour)
+	choicething = thing.objects.filter(subType = thingtype, sliprating__gte = float(srating), generalColour = gcolour).order_by(choice).last()
+	return JsonResponse(ThingSerializer_JustName(choicething).data)
